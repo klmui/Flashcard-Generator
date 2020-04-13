ffd59153d250ed183c7cbce6caad499f011ff936
Kenneth Mui <klmui@wisc.edu>
2020-04-13 00:06:09 -0500

    Add card styling: shadow, hover, flip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FlashcardList from './FlashcardList';
+import './app.css';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
