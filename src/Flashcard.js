46fc2d345d9738376860d588fa1150cb783b17f4
Kenneth Mui <klmui@wisc.edu>
2020-04-12 23:41:25 -0500

    Implement flip logic using states

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Flashcard() {
+export default function Flashcard({ flashcard }) {
+  // By default set to false because we want to show front side
+  // useState(false) sets flip to false and setFlip is a method
+  const [flip, setFlip] = useState(false);
+
   return (
-    <div>
-      
+    <div onClick={() => setFlip(!flip)}>
+      {flip ? flashcard.answer : flashcard.question}
     </div>
   )
 }
