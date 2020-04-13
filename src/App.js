90247e89a149bcfd968f17efdff6e7bcbcffb867
Kenneth Mui <klmui@wisc.edu>
2020-04-12 23:25:35 -0500

    Add sample data

--- src/App.js
+++ src/App.js
@@ -6,4 +6,29 @@ function App() {
   );
 }
 
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2 + 2?",
+    answer: "4",
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    id: 2,
+    question: "Queston 2?",
+    answer: "4",
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }
+]
+
 export default App;
