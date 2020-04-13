4f10867c3902a460c8794d3480a806ad39eecc22
Kenneth Mui <klmui@wisc.edu>
2020-04-13 00:30:11 -0500

    Get data and present it on card

--- src/App.js
+++ src/App.js
@@ -8,8 +8,32 @@ function App() {
 
   // useEffect hook for whenever the page load, empty array as soon as component mounts
   useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=10')
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        // Call res.data (results is part of obj)
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = questionItem.correct_answer;
+          // Incorrect answers at front and then correct answer last
+          // Spread operator makes deep copy and concats
+          const options = [...questionItem.incorrect_answers, answer];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            // 50% of time we get .5 and 50% is -.5
+            options: options.sort(() => Math.random() - .5)
+          }
+        }));
+        console.log(res.data);
+      });
   }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
 
   return (
     <FlashcardList flashcards={flashcards} />
